0fb803f1416507080b834afe63715b7d36783356
Harry Rhodes <hjtrhodes@gmail.com>
2023-12-14 11:29:07 +0000

    login and signup modal so far

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LoginModal from '../components/LoginSignupModal/LoginSignupModal';
+
+const Home = () => {
+
+    return (
+        <>
+        <div>
+            <h1>This is the homepage</h1>
+            <LoginModal />
+        </div>
+        </>
+    )
+};
+
+export default Home
